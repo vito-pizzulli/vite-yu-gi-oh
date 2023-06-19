6bb792b6f5e8651942ba87351613688ba88a67e4
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-19 16:29:32 +0200

    imported axios & store.js in AppMain, created API call and response saved to yugiohApi variable in store.js

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -6,17 +6,35 @@
 
 <script>
 import AppCardsList from './AppCardsList.vue';
+import axios from 'axios';
+import { store } from '../store.js';
 export default {
     name: 'AppMain',
 
     components: {
         AppCardsList
+    },
+
+    data() {
+        return {
+            store
+        }
+    },
+
+    created(){
+        axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0')
+            .then(function (response) {
+                console.log(response);
+                store.yugiohApi = response
+            })
+            .catch(function (error) {
+                console.log(error);
+            })
     }
 }
 </script>
 
 <style lang="scss" scoped>
-    @use '../styles/general' as *;
     @use '../styles/partials/variables' as *;
     @use '../styles/partials/mixins' as *;
 
