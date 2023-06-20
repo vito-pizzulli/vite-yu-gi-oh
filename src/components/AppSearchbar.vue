c5e567f68b3e938232bd9e64e9423b129ff3a697
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-20 11:54:35 +0200

    created AppSearchbar.vue component for cards filtering

---
+++ src/components/AppSearchbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app-searchbar">
+        <h1>prova</h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppSearchbar'
+}
+</script>
+<style lang="scss" scoped>
+    
+</style>
