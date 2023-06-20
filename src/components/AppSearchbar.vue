0a2637ad7277e69d61c5a1f4bec80d7b7118615e
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-20 12:25:57 +0200

    added select with options dynamically generated from archetype array passed from parent with prop

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -1,13 +1,28 @@
 <template>
     <div class="app-searchbar">
-        <h1>prova</h1>
+        <select name="card-archetype" id="card-archetype">
+            <option v-for="archetype in archetypeList" :value="archetype.archetype_name">{{ archetype.archetype_name }}</option>
+        </select>
+
+
     </div>
 </template>
 <script>
 export default {
-    name: 'AppSearchbar'
+    name: 'AppSearchbar',
+
+    props: {
+        archetypeList: Array
+    }
 }
 </script>
 <style lang="scss" scoped>
+    @use '../styles/partials/variables' as *;
+    @use '../styles/partials/mixins' as *;
     
+    div.app-searchbar {
+        width: 80%;
+        margin: 0 auto;
+        padding: 1rem;
+    }
 </style>
