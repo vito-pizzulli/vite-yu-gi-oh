40556a3ad605e3738071c799929036392f6f6b43
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-06-20 14:55:54 +0200

    modified archetype change event name, added textbox with button to search a card by name, passing value by emit

--- src/components/AppSearchbar.vue
+++ src/components/AppSearchbar.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="app-searchbar">
-        <select name="card-archetype" id="card-archetype" @change="$emit('archetypeFilterChosen', $event.target.value)">
+        <select name="card-archetype" id="card-archetype" @change="$emit('eventArchetypeChange', $event.target.value)">
             <option selected hidden>Choose an Archetype</option>
             <option v-for="archetype in archetypeList">{{ archetype.archetype_name }}</option>
         </select>
-
+        <div class="card-name-search">
+            <input type="text" placeholder="Insert card name" v-model="nameToSearch" @keyup.enter="$emit('eventNameSearch', nameToSearch)">
+            <button @click="$emit('eventNameSearch', nameToSearch)">Search</button>
+        </div>
 
     </div>
 </template>
@@ -14,6 +17,12 @@ export default {
 
     props: {
         archetypeList: Array
+    },
+
+    data() {
+        return {
+            nameToSearch: ''
+        }
     }
 }
 </script>
@@ -22,6 +31,7 @@ export default {
     @use '../styles/partials/mixins' as *;
     
     div.app-searchbar {
+        @include flex(row, space-between, center, wrap);
         width: 80%;
         margin: 0 auto;
         padding: 1rem;
@@ -33,6 +43,24 @@ export default {
             border-radius: 10px;
             border: none;
             text-align: center;
+        }
+
+        div.card-name-search {
+            input {
+                padding: .5rem 1rem;
+                font-size: 1rem;
+                border-radius: 10px;
+                border: none;
+                margin: 0 1rem;
+            }
+
+            button {
+                padding: .5rem 1rem;
+                border-radius: 20px;
+                border: none;
+                background-color: white;
+                font-size: 1rem;
+            }
         }
     }
 </style>
